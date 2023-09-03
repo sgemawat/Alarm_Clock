cc0e2f61f24b9780b3b5ccaf10b7afe60bc5c766
Saransh Gemawat <sgemawat29@gmail.com>
2023-09-04 05:28:24 +0530

    script.js

--- script.js
+++ script.js
@@ -1,81 +1,141 @@
-// Selecting DOM elements
-const currentTime = document.querySelector("h1"); // Current time display
-const content = document.querySelector(".content"); // Content container
-const selectMenu = document.querySelectorAll("select"); // Select dropdowns
-const setAlarmBtn = document.querySelector("button"); // Set Alarm button
+// Document Object Model (DOM) element references
+const currentTime = document.querySelector("h1"), // Reference to the element displaying the current time
+  content = document.querySelector(".content"), // Reference to the content section
+  hourSelect = document.getElementById("hour"), // Reference to the hour select element
+  minuteSelect = document.getElementById("minute"), // Reference to the minute select element
+  ampmSelect = document.getElementById("ampm"), // Reference to the AM/PM select element
+  setAlarmBtn = document.getElementById("setAlarm"), // Reference to the "Set Alarm" button
+  clearAlarmsBtn = document.getElementById("clearAlarms"), // Reference to the "Clear All Alarms" button
+  alarmsContainer = document.getElementById("alarms"); // Reference to the container for displaying alarms
 
 // Initialize variables
-let alarmTime, isAlarmSet;
-const ringtone = new Audio("./ringtone.mp3"); // Audio element for the alarm ringtone
+let alarms = []; // Array to store alarm times
+let alertShown = false; // Flag to track whether the alert has been shown
 
 // Populate the hour dropdown with options from 12 to 1
 for (let i = 12; i > 0; i--) {
-    i = i < 10 ? `0${i}` : i;
-    let option = `<option value="${i}">${i}</option>`;
-    selectMenu[0].firstElementChild.insertAdjacentHTML("afterend", option);
+  let option = `<option value="${i < 10 ? '0' + i : i}">${i < 10 ? '0' + i : i}</option>`;
+  hourSelect.insertAdjacentHTML("beforeend", option);
 }
 
-// Populate the minute dropdown with options from 59 to 0
-for (let i = 59; i >= 0; i--) {
-    i = i < 10 ? `0${i}` : i;
-    let option = `<option value="${i}">${i}</option>`;
-    selectMenu[1].firstElementChild.insertAdjacentHTML("afterend", option);
+// Populate the minute dropdown with options from 0 to 59
+for (let i = 0; i <= 59; i++) {
+  let option = `<option value="${i < 10 ? '0' + i : i}">${i < 10 ? '0' + i : i}</option>`;
+  minuteSelect.insertAdjacentHTML("beforeend", option);
 }
 
 // Populate the AM/PM dropdown with "AM" and "PM" options
-for (let i = 2; i > 0; i--) {
-    let ampm = i == 1 ? "AM" : "PM";
-    let option = `<option value="${ampm}">${ampm}</option>`;
-    selectMenu[2].firstElementChild.insertAdjacentHTML("afterend", option);
+ampmSelect.innerHTML = '<option value="AM">AM</option><option value="PM">PM</option>';
+
+// Function to update the current time displayed
+function updateCurrentTime() {
+  const date = new Date();
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  const ampm = h >= 12 ? "PM" : "AM";
+  h = h % 12 || 12;
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+  currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+
+  // Check if any alarms match the current time and the alert hasn't been shown
+  const currentAlarmTime = `${h}:${m} ${ampm}`;
+  if (alarms.includes(currentAlarmTime) && !alertShown) {
+    alert(`Alarm: ${currentAlarmTime}`);
+    alertShown = true; // Set the flag to true to indicate that the alert has been shown
+  } else if (!alarms.includes(currentAlarmTime)) {
+    // Reset the alert flag if the current time no longer matches any alarms
+    alertShown = false;
+  }
 }
 
-// Update the current time every second
-setInterval(() => {
-    let date = new Date(),
-    h = date.getHours(),
-    m = date.getMinutes(),
-    s = date.getSeconds(),
-    ampm = "AM";
-    if(h >= 12) {
-        h = h - 12;
-        ampm = "PM";
-    }
-    h = h == 0 ? h = 12 : h;
-    h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
-    s = s < 10 ? "0" + s : s;
-    currentTime.innerText = `${h}:${m}:${s} ${ampm}`;
+// Function to create an alarm element
+function createAlarmElement(time) {
+  const alarmElement = document.createElement("div");
+  alarmElement.classList.add("alarm");
 
-    // Check if the alarm time matches the current time
-    if (alarmTime === `${h}:${m} ${ampm}`) {
-        ringtone.play(); // Play the alarm ringtone
-        ringtone.loop = true; // Set the ringtone to loop
+  // Create a delete button for the alarm
+  const deleteButton = document.createElement("span");
+  deleteButton.classList.add("alarm-delete-button");
+  deleteButton.innerText = "Delete";
+
+  // Event listener to delete the alarm when the button is clicked
+  deleteButton.addEventListener("click", () => {
+    const index = alarms.indexOf(time);
+    if (index !== -1) {
+      deleteAlarm(index);
     }
+  });
+
+  // Display the time and the delete button
+  alarmElement.innerText = time;
+  alarmElement.appendChild(deleteButton);
+
+  // Append the alarm element to the alarms container
+  alarmsContainer.appendChild(alarmElement);
+}
+
+// Function to add a new alarm
+function addAlarm() {
+  const selectedHour = hourSelect.value;
+  const selectedMinute = minuteSelect.value;
+  const selectedAmPm = ampmSelect.value;
+
+  if (selectedHour === "Hour" || selectedMinute === "Minute") {
+    alert("Please select a valid time for the alarm.");
+    return;
+  }
+
+  // Format the new alarm time
+  const newAlarm = `${selectedHour}:${selectedMinute} ${selectedAmPm}`;
+  alarms.push(newAlarm);
+
+  // Create and display the new alarm element
+  createAlarmElement(newAlarm);
+  updateLocalStorage();
+  resetSelects();
+}
+
+// Function to reset the select boxes
+function resetSelects() {
+  hourSelect.selectedIndex = 0;
+  minuteSelect.selectedIndex = 0;
+  ampmSelect.selectedIndex = 0;
+}
+
+// Function to delete an alarm
+function deleteAlarm(index) {
+  alarms.splice(index, 1);
+  alarmsContainer.removeChild(alarmsContainer.children[index]);
+  updateLocalStorage();
+}
+
+// Function to load alarms from local storage
+function loadAlarmsFromLocalStorage() {
+  const storedAlarms = localStorage.getItem("alarms");
+  if (storedAlarms) {
+    alarms = JSON.parse(storedAlarms);
+    alarms.forEach((alarm) => createAlarmElement(alarm));
+  }
+}
+
+// Function to update local storage with alarms
+function updateLocalStorage() {
+  localStorage.setItem("alarms", JSON.stringify(alarms));
+}
+
+// Event listeners
+setAlarmBtn.addEventListener("click", addAlarm);
+clearAlarmsBtn.addEventListener("click", () => {
+  alarms = [];
+  alarmsContainer.innerHTML = "";
+  updateLocalStorage();
 });
 
-// Function to set or clear the alarm
-function setAlarm() {
-    if (isAlarmSet) {
-        alarmTime = "";
-        ringtone.pause(); // Pause the alarm ringtone
-        content.classList.remove("disable"); // Remove the "disable" class from content
-        setAlarmBtn.innerText = "Set Alarm"; // Update button text
-        return isAlarmSet = false; // Reset the alarm status
-    }
-
-    // Get the selected time from dropdowns
-    let time = `${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}`;
-
-    // Check if a valid time is selected
-    if (time.includes("Hour") || time.includes("Minute") || time.includes("AM/PM")) {
-        return alert("Please, select a valid time to set Alarm!"); // Display an alert for invalid time
-    }
-
-    alarmTime = time; // Set the alarm time
-    isAlarmSet = true; // Set the alarm status to true
-    content.classList.add("disable"); // Add the "disable" class to content
-    setAlarmBtn.innerText = "Clear Alarm"; // Update button text
-}
-
-// Add a click event listener to the Set Alarm button
-setAlarmBtn.addEventListener("click", setAlarm);
+// Load alarms from local storage and update current time
+loadAlarmsFromLocalStorage();
+updateCurrentTime();
+// Update current time periodically
+setInterval(updateCurrentTime, 1000);
